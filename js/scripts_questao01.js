6f50b29b4478707f2301a5adf06578e8f615669e
Eloisa Zeferino <eloisa.zeferino@aluno.senai.br>
2026-06-12 16:47:14 -0300

    Criada a função de calculo de media

--- js/scripts_questao01.js
+++ js/scripts_questao01.js
@@ -1,26 +1,19 @@
 // PEGANDO ELEMENTOS DO DOM
-
 const formDados = document.querySelector('#formulario-pessoa')
 const divResultado = document.querySelector('#div-dados')
 
 // CAPTANDO O EVENTO SUBMIT DO FORMULÁRIO
-
 formDados.addEventListener('submit', (evt)=>{
     evt.preventDefault()
 
     const objformDados = new FormData(formDados)
 
-    let nome = objformDados.get('nome')
-    let idade = objformDados.get('idade')
+    let n1 = parseFloat(objformDados.get('num1'))
+    let n2 = parseFloat(objformDados.get('num2'))
+    let n3 = parseFloat(objformDados.get('num3'))
 
-    let situacaoIdade = ''
+    let media = (n1 + n2 + n3)/3
 
-    if(idade >= 18){
-        situacaoIdade = `${nome}, você é maior de idade`
-    }else{
-        situacaoIdade = `${nome}, você é menor de idade`
-    }
-
-    divResultado.innerHTML = situacaoIdade
-
+    divResultado.innerHTML = `A MEDIA DOS NUMEROS DIGITADOS É ${media.toFixed(2).replace('.',',')}`
+    formDados.reset
 })
