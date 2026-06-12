a93cc2213c3d671f702f89efdb892ce547bf6779
Eloisa Zeferino <eloisa.zeferino@aluno.senai.br>
2026-06-12 15:49:06 -0300

    Corrigido link entre index.html e script_idade.js

--- js/script_idade.js
+++ js/script_idade.js
@@ -1,4 +1,26 @@
 // PEGANDO ELEMENTOS DO DOM
 
 const formDados = document.querySelector('#formulario-pessoa')
-const divResultado = document.querySelector('#div-Dados')
+const divResultado = document.querySelector('#div-dados')
+
+// CAPTANDO O EVENTO SUBMIT DO FORMULÁRIO
+
+formDados.addEventListener('submit', (evt)=>{
+    evt.preventDefault()
+
+    const objformDados = new FormData(formDados)
+
+    let nome = objformDados.get('nome')
+    let idade = objformDados.get('idade')
+
+    let situacaoIdade = ''
+
+    if(idade >= 18){
+        situacaoIdade = `${nome}, você é maior de idade`
+    }else{
+        situacaoIdade = `${nome}, você é menor de idade`
+    }
+
+    divResultado.innerHTML = situacaoIdade
+
+})
